c874b9a7a73ba9969509a129670e5bbb19d2c7d8
otavio-f <38397566+otavio-f@users.noreply.github.com>
2024-07-14 13:08:19 -0300

    refactor: Realocate custom parameters to config file
    Custom app parameters moved to "app_params.h" file

--- InkClock-ESP.ino
+++ InkClock-ESP.ino
@@ -2,11 +2,9 @@
 #include "clock.h"
 #include "wificonn.h"
 #include "webserver.h"
-#include "secrets.h" // define WIFI_SSID and WIFI_PW
+#include "app_params.h" // APP_... constants defined here
 
 #define TZ_BR -10800
-#define SRV_ADDR "esp32" // webserver address
-#define SRV_PORT 80 // webserver port
 
 char timenow[64] = {0};
 
@@ -18,7 +16,7 @@ setup()
 {
   Serial.begin(115200);
   Serial.print("\n\nConnecting to wifi... ");
-  int wifistatus = wifi_connect(WIFI_SSID, WIFI_PW, 15);
+  int wifistatus = wifi_connect(APP_WIFI_SSID, APP_WIFI_PW, 15);
   if(wifistatus == CONN_OK)
     Serial.printf("Connected (%s).\n", get_local_ip().c_str());
   else
@@ -27,10 +25,10 @@ setup()
   sync_time("pool.ntp.org", TZ_BR);
   get_time(timenow, sizeof(timenow), "%d/%m/%Y\n%H:%M");
   Serial.printf("time: %s, mins: %d, secs: %d\n", timenow, get_now_mins(), get_now_secs());
-  switch(initialize_server(SRV_ADDR, SRV_PORT))
+  switch(initialize_server(APP_SRV_MDNS_NAME, APP_SRV_PORT))
   {
     case SRV_OK:
-      Serial.printf("Server initialized on port %d\n", SRV_PORT);
+      Serial.printf("Server initialized on port %d\n", APP_SRV_PORT);
       break;
     case SRV_FAIL:
       Serial.println("Failed to initialize server");
